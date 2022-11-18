6243fd62b1137feb77cec27f5937dbf6ca528c97
Victor <matheus0588@outlook.com>
2022-11-18 16:56:23 -0300

    req05

---
+++ src/Archetypes/Ranger.ts
@@ -0,0 +1,21 @@
+import { EnergyType } from '../Energy';
+import Archetype from './Archetype';
+
+export default class Mage extends Archetype {
+  private _energyType: EnergyType;
+  static archetypeInstances = 0;
+
+  constructor(name: string) {
+    super(name);
+    this._energyType = 'stamina';
+  }
+
+  static createdArchetypeInstances(): number {
+    this.archetypeInstances += 1;
+    return this.archetypeInstances;
+  }
+
+  get energyType(): EnergyType {
+    return this._energyType;
+  }
+}
